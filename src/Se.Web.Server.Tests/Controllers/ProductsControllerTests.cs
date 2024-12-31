01f7b4e890e75c16eb08a3dc29529a45bfb5e832
David <witam@tuta.io>
2024-12-31 23:38:50 +0100

    test: UT ProductsController.GetAll

---
+++ src/Se.Web.Server.Tests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Se.Web.Server.Controllers;
+
+namespace Se.Web.Server.Tests.Controllers;
+
+public class ProductsControllerTests
+{
+    private readonly ProductsController _sut;
+    
+    private readonly ILogger<ProductsController> _loggerMock = Substitute.For<ILogger<ProductsController>>();
+
+    public ProductsControllerTests()
+    {
+        _sut = new ProductsController(_loggerMock);
+    }
+
+    [Fact]
+    public void GetAll_ShouldReturnResponse()
+    {
+        // Act
+        var result = _sut.GetAll();
+        
+        Assert.Empty(result);
+    }
+}
