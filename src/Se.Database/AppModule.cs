bf90f79530b7b7b03f1e6d4824569377bad5b607
David <witam@tuta.io>
2025-01-03 21:03:45 +0100

    feat: Database module registration

---
+++ src/Se.Database/AppModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Se.Application.Features.Products;
+using Se.Database.Repositories;
+
+namespace Se.Database;
+
+public static class AppModule
+{
+    public static IServiceCollection AddDatabaseModule(this IServiceCollection services)
+    {
+        services.AddSingleton<IProductRepo, ProductMemoryRepo>();
+        
+        return services;
+    }
+}
--- src/Se.Web.Server/Program.cs
+++ src/Se.Web.Server/Program.cs
@@ -1,6 +1,10 @@
+using Se.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDatabaseModule();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
