a3e3470b8c937ae3b7d2a2dee7d612661389a3ac
David <witam@tuta.io>
2025-01-03 21:03:08 +0100

    feat: Repo id change to int

--- src/Se.Application/Base/Database/ICrudRepo.cs
+++ src/Se.Application/Base/Database/ICrudRepo.cs
@@ -1,11 +1,11 @@
 ﻿namespace Se.Application.Base.Database;
 
-public interface ICrudRepo<TEntity, TId>
+public interface ICrudRepo<TEntity>
 {
-    Task<TEntity?> GetAsync(TId id);
+    Task<TEntity?> GetAsync(int id);
     Task<IEnumerable<TEntity>> GetAllAsync();
 
-    Task<TId> AddAsync(TEntity entity);
+    Task<int> AddAsync(TEntity entity);
     Task UpdateAsync(TEntity entity);
-    Task DeleteAsync(TId entity);
+    Task DeleteAsync(int entity);
 }
--- src/Se.Application/Features/Products/IProductRepo.cs
+++ src/Se.Application/Features/Products/IProductRepo.cs
@@ -4,4 +4,4 @@ using Se.Domain.Features.Products;
 namespace Se.Application.Features.Products;
 
 public interface IProductRepo 
-    : ICrudRepo<ProductEntity, int>;
+    : ICrudRepo<ProductEntity>;
