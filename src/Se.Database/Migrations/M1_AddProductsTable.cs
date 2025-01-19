bc13bcff82b118486691e47fa1387548c2d89a99
David <witam@tuta.io>
2025-01-19 21:08:12 +0100

    feat: Migration adding table

---
+++ src/Se.Database/Migrations/M1_AddProductsTable.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Se.Database.Migrations;
+
+[Migration(1)]
+public class M1_AddProductsTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Products")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("Name").AsString(255).NotNullable();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Products");
+    }
+}
