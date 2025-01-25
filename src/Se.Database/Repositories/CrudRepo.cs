bdc8d7c9ba6a4289658e5b3444f46ef8b42a93a1
David <witam@tuta.io>
2025-01-25 12:25:32 +0100

    feat: CrudRepo init

---
+++ src/Se.Database/Repositories/CrudRepo.cs
@@ -0,0 +1,32 @@
+﻿using Se.Application.Base.Database;
+using Se.Application.Base.Database.GetAll;
+
+namespace Se.Database.Repositories;
+
+public class CrudRepo<TEntity> : ICrudRepo<TEntity>
+{
+    public Task<TEntity?> GetAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetAllResultDto> GetAllAsync(GetAllArgsDto query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> AddAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteManyAsync(IReadOnlyCollection<int> ids)
+    {
+        throw new NotImplementedException();
+    }
+}
