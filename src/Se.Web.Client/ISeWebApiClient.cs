d57e103a22d6e7a51833e6ab3954eab6633683a2
David <witam@tuta.io>
2025-02-06 22:30:34 +0100

    refactor: Rename api client interface name

--- src/Se.Web.Client/ISeWebApiClient.cs
+++ src/Se.Web.Client/ISeWebApiClient.cs
@@ -3,7 +3,7 @@ using Se.Contracts.Shared.Crud.GetDetails;
 
 namespace Se.Web.Client;
 
-public interface IWebApiClient
+public interface ISeWebApiClient
 {
     Task<ProductGetDetailsResponse> GetDetailsAsync(GetDetailsRequest request, CancellationToken ct = default);
 }
