6439d34698e397ee5b3c9369cd0e60aad77f6ae0
David <witam@tuta.io>
2025-02-09 15:36:54 +0100

    feat: Configure gui DI

--- src/Se.Web.Gui/Program.cs
+++ src/Se.Web.Gui/Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Se.Web.Client;
 using Se.Web.Gui;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseAddress = builder.HostEnvironment.BaseAddress;
+
+#if DEBUG
+
+baseAddress = "https://localhost:5443";
+
+#endif
+
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+builder.Services.AddScoped<ISeWebApiClient, SeWebApiClient>();
 
 await builder.Build().RunAsync();
