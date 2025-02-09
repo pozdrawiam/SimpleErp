baa28300f7164bb60160272e58533718b6a96c2e
David <witam@tuta.io>
2025-02-09 21:26:21 +0100

    refactor: JsonApiClient

--- src/Se.Web.Client/SeWebApiClient.cs
+++ src/Se.Web.Client/SeWebApiClient.cs
@@ -1,75 +1,16 @@
-﻿using System.Net;
-using System.Net.Http.Json;
-using Se.Contracts.Features.Products;
+﻿using Se.Contracts.Features.Products;
 using Se.Contracts.Shared.Crud.GetDetails;
+using Se.Web.Client.Shared;
 
 namespace Se.Web.Client;
 
-public class SeWebApiClient : ISeWebApiClient
+public class SeWebApiClient : JsonApiClient, ISeWebApiClient
 {
-    private readonly HttpClient _http;
-
-    public SeWebApiClient(HttpClient http)
+    public SeWebApiClient(HttpClient httpClient) 
+        : base(httpClient)
     {
-        _http = http;
     }
-    
-    public async Task<ProductGetDetailsResponse?> ProductGetDetailsAsync(GetDetailsRequest request, CancellationToken ct = default)
-    {
-        return await GetAsync<ProductGetDetailsResponse>($"Products/GetDetails?Id={request.Id}");
-    }
-    
-    private async Task<TResult?> GetAsync<TResult>(string url) 
-        //where TResult : new() 
-        => await SendAsync<object, TResult?>(HttpMethod.Get, url);
-    
-    private async Task<TResult?> SendAsync<TData, TResult>(
-        HttpMethod method, 
-        string url, 
-        TData? data = default)
-        //where TResult : new()
-    {
-        var request = new HttpRequestMessage(method, $"api/{url}");
-        
-        if (data != null && (method == HttpMethod.Post || method == HttpMethod.Put))
-        {
-            request.Content = JsonContent.Create(data);
-        }
 
-        HttpResponseMessage response;
-        
-        try
-        {
-            response = await _http.SendAsync(request);
-        }
-        catch (HttpRequestException e) when (e.StatusCode == null)
-        {
-            return default;
-        }
-
-        if (response.IsSuccessStatusCode)
-            return await response.Content.ReadFromJsonAsync<TResult>();
-
-        if (response.StatusCode == HttpStatusCode.BadRequest)
-        {
-            //AppValidationResult? validationResult = null;
-            try
-            {
-                //validationResult = await response.Content.ReadFromJsonAsync<AppValidationResult>();
-            }
-            catch
-            {
-                // ignored
-            }
-
-            //if (validationResult != null)
-                //throw new AppValidationException(validationResult);
-        }
-        else if (response.StatusCode == HttpStatusCode.Unauthorized)
-        {
-            //await OnUnauthorized();
-        }
-
-        return default;
-    }
+    public async Task<ProductGetDetailsResponse?> ProductGetDetailsAsync(GetDetailsRequest request, CancellationToken ct = default) 
+        => await GetAsync<ProductGetDetailsResponse>($"Products/GetDetails?Id={request.Id}");
 }
---
+++ src/Se.Web.Client/Shared/JsonApiClient.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+namespace Se.Web.Client.Shared;
+
+public abstract class JsonApiClient
+{
+    private readonly HttpClient _httpClient;
+
+    protected JsonApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    protected async Task<TResult?> GetAsync<TResult>(string url) 
+        => await SendAsync<object, TResult?>(HttpMethod.Get, url);
+    
+    private async Task<TResult?> SendAsync<TData, TResult>(
+        HttpMethod method, 
+        string url, 
+        TData? data = default)
+    {
+        var request = new HttpRequestMessage(method, $"api/{url}");
+        
+        if (data != null && (method == HttpMethod.Post || method == HttpMethod.Put))
+        {
+            request.Content = JsonContent.Create(data);
+        }
+
+        HttpResponseMessage response;
+        
+        try
+        {
+            response = await _httpClient.SendAsync(request);
+        }
+        catch (HttpRequestException e) when (e.StatusCode == null)
+        {
+            return default;
+        }
+
+        if (response.IsSuccessStatusCode)
+            return await response.Content.ReadFromJsonAsync<TResult>();
+
+        if (response.StatusCode == HttpStatusCode.BadRequest)
+        {
+            //AppValidationResult? validationResult = null;
+            try
+            {
+                //validationResult = await response.Content.ReadFromJsonAsync<AppValidationResult>();
+            }
+            catch
+            {
+                // ignored
+            }
+
+            //if (validationResult != null)
+            //throw new AppValidationException(validationResult);
+        }
+        else if (response.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            //await OnUnauthorized();
+        }
+
+        return default;
+    }
+}
