879291249cf285211692fc4e99e627e57920fec2
Pozdrawiam <witam@tuta.io>
2025-02-23 22:25:02 +0100

    feat: OrderDeleteCmdHandler

---
+++ src/Se.Application/Features/Orders/OrderDeleteCmdHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Se.Application.Features.Orders;
+
+public class OrderDeleteCmdHandler
+{
+    private readonly IOrderRepo _orderRepo;
+
+    public OrderDeleteCmdHandler(IOrderRepo orderRepo)
+    {
+        _orderRepo = orderRepo;
+    }
+    
+    public async Task<int> Handle(OrderDeleteCmd cmd)
+    {
+        var order = await _orderRepo.GetAsync(cmd.Id);
+        
+        if (order == null)
+            return 0;
+
+        await _orderRepo.DeleteManyAsync([cmd.Id]);
+        
+        return order.Id;
+    }
+}
