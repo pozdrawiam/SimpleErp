a36f586b384d615810afc7e33f352009db89bab8
Pozdrawiam <witam@tuta.io>
2025-02-24 21:42:59 +0100

    refactor: Move OrderItemDto to a separate file

--- src/Se.Contracts/Features/Orders/OrderCreateRequest.cs
+++ src/Se.Contracts/Features/Orders/OrderCreateRequest.cs
@@ -5,9 +5,3 @@ public class OrderCreateRequest
     public string Note { get; set; } = "";
     public List<OrderItemDto> Items { get; set; } = [];
 }
-
-public class OrderItemDto
-{
-    public int ProductId { get; set; }
-    public decimal Quantity { get; set; } = 1M;
-}
---
+++ src/Se.Contracts/Features/Orders/OrderItemDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Se.Contracts.Features.Orders;
+
+public class OrderItemDto
+{
+    public int ProductId { get; set; }
+    public decimal Quantity { get; set; } = 1M;
+}
