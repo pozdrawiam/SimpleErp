f36977eb5c40f2e1fea2e3f68306ca00b30870c6
Pozdrawiam <witam@tuta.io>
2025-02-24 21:38:25 +0100

    feat: Introduce ICmd interface and update DeleteManyRequest to implement it

---
+++ src/Se.Contracts/Shared/Cqs/ICmd.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Se.Contracts.Shared.Cqs;
+
+public interface ICmd : IRequest<int>;
+
+public interface ICmd<out TCmdResult> : IRequest<TCmdResult>;
--- src/Se.Contracts/Shared/Crud/DeleteMany/DeleteManyRequest.cs
+++ src/Se.Contracts/Shared/Crud/DeleteMany/DeleteManyRequest.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Se.Contracts.Shared.Cqs;
 
 namespace Se.Contracts.Shared.Crud.DeleteMany;
 
-public record DeleteManyRequest
+public record DeleteManyRequest : ICmd
 {
     [Required]
     public IReadOnlyCollection<int>? Ids { get; init; }
