27e56b64e959a80256fb7766f2afb7520e5527da
David <witam@tuta.io>
2025-02-26 20:06:50 +0100

    feat: Order delete repo method

---
+++ src/Se.Database/Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Se.Database.Models;
+
+[Table("Orders")]
+public class OrderModel
+{
+    public int Id { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public string Note { get; set; } = "";
+}
--- src/Se.Database/Repositories/OrderRepo.cs
+++ src/Se.Database/Repositories/OrderRepo.cs
@@ -1,11 +1,23 @@
-﻿using Se.Application.Shared;
+﻿using System.Data;
+using System.Transactions;
+using Dapper.Contrib.Extensions;
+using Se.Application.Shared;
 using Se.Contracts.Shared.Crud.QueryAll;
+using Se.Database.DbConnection;
+using Se.Database.Models;
 using Se.Domain.Features.Orders;
 
 namespace Se.Database.Repositories;
 
 public class OrderRepo : ICrudRepo<OrderEntity>
 {
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public OrderRepo(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+    
     public Task<OrderEntity?> GetAsync(int id)
     {
         throw new NotImplementedException();
@@ -26,8 +38,29 @@ public class OrderRepo : ICrudRepo<OrderEntity>
         throw new NotImplementedException();
     }
 
-    public Task DeleteManyAsync(IReadOnlyCollection<int> ids)
+    public async Task DeleteManyAsync(IReadOnlyCollection<int> ids)
     {
-        throw new NotImplementedException();
+        using var connection = CreateOpenConnection();
+        using var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+        foreach (var id in ids)
+        {
+            var entity = await connection.GetAsync<OrderModel>(id);
+
+            if (entity != null)
+            {
+                await connection.DeleteAsync(entity);
+            }
+        }
+
+        transactionScope.Complete();
+    }
+    
+    private IDbConnection CreateOpenConnection()
+    {
+        var dbConnection = _dbConnectionFactory.CreateConnection();
+        dbConnection.Open();
+
+        return dbConnection;
     }
 }
