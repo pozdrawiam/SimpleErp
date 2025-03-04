48a1877f2a06713af517f7a9b9c8b6db1dcb8d42
Pozdrawiam <witam@tuta.io>
2025-03-04 20:16:45 +0100

    feat: Add OrderItems and Orders tables with migrations

---
+++ src/Se.Database/Migrations/M2_AddOrdersTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace Se.Database.Migrations;
+
+[Migration(2)]
+public class M2_AddOrdersTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Orders")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("CreatedAtUtc").AsDateTime()
+            .WithColumn("Note").AsString().NotNullable();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Orders");
+    }
+}
---
+++ src/Se.Database/Migrations/M3_AddOrderItemsTable.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace Se.Database.Migrations;
+
+[Migration(3)]
+public class M3_AddOrderItemsTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("OrderItems")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("OrderId").AsInt32().ForeignKey("Orders", "Id")
+            .WithColumn("Guid").AsGuid()
+            .WithColumn("ProductId").AsInt32().ForeignKey("Products", "Id")
+            .WithColumn("Quantity").AsDecimal();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("OrderItems");
+    }
+}
