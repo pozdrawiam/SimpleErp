99cc8c7f4feac2edd7aa63d614cd9fda950507df
Pozdrawiam <witam@tuta.io>
2025-03-06 20:37:18 +0100

    feat: Add IQuery and IQueryHandler interfaces for MediatR integration

---
+++ src/Se.Application/Shared/IQueryHandler.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Se.Contracts.Shared.Cqs;
+
+namespace Se.Application.Shared;
+
+public interface IQueryHandler<in TQuery, TQueryResult> : IRequestHandler<TQuery, TQueryResult>
+    where TQuery : IQuery<TQueryResult>;
---
+++ src/Se.Contracts/Shared/Cqs/IQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Se.Contracts.Shared.Cqs;
+
+public interface IQuery<out TResult> : IRequest<TResult>;
