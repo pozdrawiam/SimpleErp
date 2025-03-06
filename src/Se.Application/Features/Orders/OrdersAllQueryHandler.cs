5cd67489cca35bf0894df051e7ca3c4523a99f9d
Pozdrawiam <witam@tuta.io>
2025-03-06 20:37:55 +0100

    feat: OrdersAllQueryHandler

---
+++ src/Se.Application/Features/Orders/OrdersAllQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Se.Application.Shared;
+using Se.Contracts.Features.Orders;
+using Se.Contracts.Shared.Crud.QueryAll;
+using Se.Domain.Features.Orders;
+
+namespace Se.Application.Features.Orders;
+
+public class OrdersAllQueryHandler : IQueryHandler<OrdersQueryAllRequest, QueryAllResponse>
+{
+    private readonly ICrudRepo<OrderEntity> _repo;
+
+    public OrdersAllQueryHandler(ICrudRepo<OrderEntity> repo)
+    {
+        _repo = repo;
+    }
+    
+    public async Task<QueryAllResponse> Handle(OrdersQueryAllRequest query, CancellationToken ct)
+    {
+        var result = await _repo.QueryAllAsync(query);
+        var response = new QueryAllResponse(result.Data, result.TotalCount);
+        
+        return response;
+    }
+}
---
+++ src/Se.Contracts/Features/Orders/OrdersQueryAllRequest.cs
@@ -0,0 +1,8 @@
+﻿using Se.Contracts.Shared.Cqs;
+using Se.Contracts.Shared.Crud.QueryAll;
+
+namespace Se.Contracts.Features.Orders;
+
+public class OrdersQueryAllRequest : QueryAllRequest, IQuery<QueryAllResponse>
+{
+}
