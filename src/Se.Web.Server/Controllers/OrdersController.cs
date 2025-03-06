27272deae299a1834814b013b3487519ffd52e64
Pozdrawiam <witam@tuta.io>
2025-03-06 20:42:56 +0100

    feat: Add QueryAll endpoint to OrdersController

--- src/Se.Web.Server/Controllers/OrdersController.cs
+++ src/Se.Web.Server/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Se.Contracts.Features.Orders;
 using Se.Contracts.Shared.Crud.Create;
+using Se.Contracts.Shared.Crud.QueryAll;
 using Se.Web.Server.Shared;
 
 namespace Se.Web.Server.Controllers;
@@ -13,6 +14,19 @@ public class OrdersController : AppApiController
     public OrdersController(IMediator mediator)
     {
         _mediator = mediator;
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<QueryAllResponse>> QueryAll(OrdersQueryAllRequest request)
+    {
+        if (!ModelState.IsValid) 
+            return BadRequest(ModelState);
+        
+        var response = await _mediator.Send(request);
+        
+        return Ok(response);
     }
     
     [HttpPost]
