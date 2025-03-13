336f64c7f0d326b9e90de47a358f0f8f88af21e9
Pozdrawiam <witam@tuta.io>
2025-03-13 19:52:49 +0100

    feat: Configure Swagger to use custom schema IDs

--- src/Se.Web.Server/Program.cs
+++ src/Se.Web.Server/Program.cs
@@ -10,7 +10,10 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Or
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.CustomSchemaIds(type => type.FullName?.Replace("+", "."));
+});
 
 var app = builder.Build();
 
