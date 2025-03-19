ede94e717b1e2a8198d1e427a62f43d072a36f70
David <witam@tuta.io>
2025-03-19 20:51:54 +0100

    feat: Add abstract CrudApiController2 class for CRUD mediator operations

---
+++ src/Se.Web.Server/Shared/CrudApiController2.cs
@@ -0,0 +1,25 @@
+﻿using Se.Contracts.Shared.Crud.Create;
+using Se.Contracts.Shared.Crud.DeleteMany;
+using Se.Contracts.Shared.Crud.GetDetails;
+using Se.Contracts.Shared.Crud.QueryAll;
+using Se.Contracts.Shared.Crud.Update;
+
+namespace Se.Web.Server.Shared;
+
+public abstract class CrudApiController2<
+    TQueryAllRequest,
+    TGetDetailsRequest,
+    TGetDetailsResponse,
+    TCreateRequest,
+    TUpdateRequest,
+    TDeleteManyRequest
+>
+    : AppApiController
+    where TQueryAllRequest : QueryAllRequest
+    where TGetDetailsRequest : GetDetailsRequest
+    where TGetDetailsResponse : GetDetailsResponseBase
+    where TCreateRequest : CreateRequestBase
+    where TUpdateRequest : UpdateRequestBase
+    where TDeleteManyRequest : DeleteManyRequest
+{
+}
