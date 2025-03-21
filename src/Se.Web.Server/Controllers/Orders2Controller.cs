fd8fe756fb616152d93e4935087e832d13b4ddcf
David <witam@tuta.io>
2025-03-21 20:14:38 +0100

    feat: Add Orders2Controller with mediator

---
+++ src/Se.Web.Server/Controllers/Orders2Controller.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Se.Contracts.Features.Orders;
+using Se.Web.Server.Shared;
+
+namespace Se.Web.Server.Controllers;
+
+public class Orders2Controller : CrudApiController2<
+    OrdersQueryAllRequest,
+    OrderGetDetailsRequest,
+    OrderGetDetailsResponse,
+    OrderCreateRequest,
+    OrderUpdateRequest,
+    OrderDeleteManyRequest
+>
+{
+    public Orders2Controller(IMediator mediator) : base(mediator)
+    {
+    }
+}
